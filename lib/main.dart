9c12f39954cff67aee1b3e067b198e03c48daf59
Ibrahim Medhat <ibrahimmedhat112@gmail.com>
2025-02-10 14:38:06 +0200

    Initialize Firebase Add firebase_options.dart Add firebase.json Add Podfile to ios and macos Update xcconfig files Initialize Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:signature_system/firebase_options.dart';
 import 'package:signature_system/src/Features/layout/view/layout_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
